dd63a25ee4b3fea10cd979f7df4df0ca4e1404f8
peibol888 <pabloalamovargas@gmail.com>
2021-10-06 15:32:31 +0000

    Finished

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 // we declare a new global variable containing an array that represents the ballons map
 // you have to add more colors into the ballonsMap array
-let ballonsMap = ['green', 'red', 'yellow', 'blue', 'blue', 'purple', 'brown', 'gray', 'yellow', 'red', 'pink', 'brown', 'red', 'yellow', 'blue', 'purple', 'brown', 'gray', 'purple', 'green'];
+let ballonsColors = ['green', 'red', 'yellow', 'blue', 'purple', 'brown', 'gray', 'pink'];
+let activeBalloons;
+let ballonsMap;
 
 // poping a balloon is basically turning his color to null (no color)
 const popBalloon = (position) => {
@@ -8,10 +10,18 @@ const popBalloon = (position) => {
     let balloon = document.querySelector(`#balloon${position}`)
     balloon.classList.remove("active");
     balloon.classList.add("popped");
+
+    ballonsMap[position] = null;
+    activeBalloons--;
+
+    if (ballonsMap.every(elem => elem === null)) render(); // true => restart the balloons
+    document.querySelector("#balloon-count").innerHTML = activeBalloons;
 }
 
 const render = () => {
-    
+    activeBalloons = 20;
+    ballonsMap = [];
+    for (let i = 0; i < 20; i++) ballonsMap.push(ballonsColors[Math.floor(Math.random()*8)]);
     // convert ballons map of colors into real html balloons
     const ballons = ballonsMap.map((color, position) => {
         return `<div id="balloon${position}" class="balloon active" onclick="popBalloon(${position});" style="background-color: ${color};"></div>`; // <--- render each balloon
@@ -19,8 +29,6 @@ const render = () => {
 
     document.querySelector("#balloon-count").innerHTML = ballons.filter(b => b !== null).length; // <-- render the balloon count into the DOM
     document.querySelector("#balloon-map").innerHTML = ballons.join(''); // <-- render the balloons into the DOM
-
-    // if(activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
 }
 
 // this makes the "render" function trigger when the website starts existing
