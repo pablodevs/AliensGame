731b3b1b3d9c5f309da8c7028b559ccaaa538d49
peibol888 <pabloalamovargas@gmail.com>
2021-10-10 13:04:23 +0000

    Aliens suben por la pantalla y mueren con click

--- src/app.js
+++ src/app.js
@@ -1,12 +1,37 @@
-const render = () => {
+const randomStart = () => {
+    let num = Math.floor(Math.random()*92);
+    return `${num}%`;
+};
 
-    // let martian = document.createElement("div");
-    martian = `<div class="martian" onclick="killMartian();"><i class="fab fa-reddit-alien alien" style="color: white;"></i></div>`
+const randomColor = (array) => {
+    return array[Math.floor(Math.random()*array.length)];
+};
 
-    document.querySelector("#container").innerHTML = martian;
+let martiansColor = ["orangered", "cyan", "deeppink", "lime", "red", "yellow"]
+let cont = 0;
+
+const renderMartian = () => {
+
+    let myMartian = document.createElement("div");
+
+    myMartian.onclick = killMartian;
+    myMartian.classList.add("alienDiv");
+    
+    let porcentaje = randomStart();
+    myMartian.style.left = porcentaje;
+
+    myMartian.innerHTML = `<i id="martian${cont}" class="fab fa-reddit-alien alien" style="color: ${randomColor(martiansColor)};"></i>`;
+
+    let myContainer = document.querySelector("#container");
+
+    myContainer.appendChild(myMartian);
+    cont++;
 }
-window.onload = render();
 
-function killMartian () {
-    document.querySelector(".martian").classList.add("killed");
+function killMartian (element) {
+    let killedMartian = document.querySelector(`#${element.target.id}`).parentNode;
+    killedMartian.parentNode.removeChild(killedMartian);
 }
+
+// Define cada cuanto tiempo (ms) aparece un martian
+var martianInterval = window.setInterval(renderMartian, 3000);
